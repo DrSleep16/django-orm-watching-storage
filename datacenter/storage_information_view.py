bfd59415fcb71b12802e5b759ca13e019768866a
Dmitry <Dr.Sleep1995@gmail.com>
2023-08-13 20:46:45 +0300

    разбил storage_information_view

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.utils import timezone
 
 
-def storage_information_view(request):
+def get_non_closed_visits():
     actives = Visit.objects.filter(leaved_at=None)
     now = timezone.localtime()
     non_closed_visits = []
@@ -17,13 +17,18 @@ def storage_information_view(request):
         formatted_hours = f"{hours:02}"
         formatted_minutes = f"{minutes:02}"
         formatted_seconds = f"{seconds:02}"
-        non_closed_visits.append({
-                'who_entered': f'{active.passcard.owner_name}',
-                'entered_at': f'''{timezone.localtime(active.entered_at).strftime('%Y-%m-%d %H:%M:%S')}''',
-                'duration': f'{formatted_hours}:{formatted_minutes}:{formatted_seconds}',
-        })
 
+        non_closed_visits.append({
+            'who_entered': active.passcard.owner_name,
+            'entered_at': f'''{timezone.localtime(active.entered_at).strftime('%Y-%m-%d %H:%M:%S')}''',
+            'duration': f'''{formatted_hours}:{formatted_minutes}:{formatted_seconds}''',
+        })
+    return non_closed_visits
+
+def storage_information_view(request):
+    non_closed_visits = get_non_closed_visits()
     context = {
         'non_closed_visits': non_closed_visits,
     }
     return render(request, 'storage_information.html', context)
+
